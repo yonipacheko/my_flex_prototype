1c2c24f215dce5f0397b601f3dad0d8d42aaa0db
yoni pacheko <fernando.lbarrajon8@gmail.com>
2013-10-30 21:24:55 +0100

    seed and DB ok

--- db/migrate/20131005044439_rename_categoryzation_to_categorization.rb
+++
@@ -1,9 +0,0 @@
-class RenameCategoryzationToCategorization < ActiveRecord::Migration
-  def up
-    rename_table :categorizations, :categorizations
-  end
-
-  def down
-    rename_table :categorizations, :categorizations
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,63 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131005051316) do
-
-  create_table "another_categorizations", force: true do |t|
-    t.integer  "video_id"
-    t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
+ActiveRecord::Schema.define(version: 20131028110618) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "categorizations", force: true do |t|
     t.integer  "video_id"
     t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "categorizations_1", force: true do |t|
-    t.integer  "video_id"
-    t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "join_of_categories", force: true do |t|
-    t.integer  "video_id"
-    t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "sqlite_sp_functions", id: false, force: true do |t|
-    t.text "name"
-    t.text "text"
-  end
-
-# Could not dump table "sqlite_stat1" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
-
-# Could not dump table "sqlite_stat3" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
-
-  create_table "sqlite_vs_links_names", id: false, force: true do |t|
-    t.text "name"
-    t.text "alias"
-  end
-
-  create_table "sqlite_vs_properties", id: false, force: true do |t|
-    t.text "parentType"
-    t.text "parentName"
-    t.text "propertyName"
-    t.text "propertyValue"
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "full_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "videos", force: true do |t|
@@ -75,8 +39,8 @@ ActiveRecord::Schema.define(version: 20131005051316) do
     t.text     "description"
     t.string   "thumb_url"
     t.string   "large_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
