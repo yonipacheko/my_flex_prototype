3c25b8fece637b8138eb9b9df0c5011cb9bb870e
yoni pacheko <fernando.lbarrajon8@gmail.com>
2013-10-31 11:48:47 +0100

    search on ui, done

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
   get '/home', to: 'videos#index'
-  resources :videos
+  resources :videos do
+    collection do
+      post '/search', to: 'videos#search'
+    end
+  end
 
   resources :categories, only: [:new, :create, :show]
 
