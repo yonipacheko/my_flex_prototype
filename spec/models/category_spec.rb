2fafc0bcf1330e5cb686bf3ddfc4bc725a4fd4a3
yoni pacheko <fernando.lbarrajon8@gmail.com>
2013-11-01 05:04:32 +0100

    recent_videos UI

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,7 +3,10 @@ require 'pry'
 
 
 describe Category do
-
+  it 'saves itself' do
+    category = Fabricate(:category)
+    expect(assigns Category.first).to eq(category)
+  end
   it { should have_many(:videos).through(:categorizations) }
 
   describe 'recent videos'  do
