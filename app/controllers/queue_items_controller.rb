5206b58700fdbea2bbc47a03e01ac52b3ae57115
yoni pacheko <fernando.lbarrajon8@gmail.com>
2013-11-12 16:38:11 +0100

    done mod5 first part

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -22,7 +22,7 @@ class QueueItemsController < ApplicationController
 
     queue_item = QueueItem.find(params[:id])
     queue_item.destroy if current_user.queue_items.include?(queue_item)
-
+    normalize_queue_items_positions
     redirect_to my_queue_path
   end
 
