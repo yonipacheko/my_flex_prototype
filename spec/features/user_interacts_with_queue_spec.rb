2df3c981cc948f756f405d34b3faeaefac5be2ca
yoni pacheko <fernando.lbarrajon8@gmail.com>
2013-11-17 18:05:35 +0100

    mod6 done

--- spec/features/user_interacts_with_queue_spec.rb
+++ spec/features/user_interacts_with_queue_spec.rb
@@ -23,15 +23,14 @@ feature 'User interacts with the queue' do
     page.should_not have_content "+ My Queue"
 
 #************************************************************
-    visit home_path
-    find("a[href='/videos/#{monk.id}']").click
-    click_link "+ My Queue"
+
+    #puts page.body
     visit home_path
     find("a[href='/videos/#{south_park.id}']").click
     click_link "+ My Queue"
     visit home_path
     find("a[href='/videos/#{futurama.id}']").click
-    puts page.body
+    #puts page.body
     click_link "+ My Queue"
 
     #this searcing with ID is not an option cuz it might interfere with the F.E
@@ -42,23 +41,43 @@ feature 'User interacts with the queue' do
 
     #Another better option will to use html5-data-tags
 
-    find("input[data-video-id='#{monk.id}']").set('3')
-    find("input[data-video-id='#{futurama.id}']").set('1')
-    find("input[data-video-id='#{south_park.id}']").set('2')
-
+    #find("input[data-video-id='#{monk.id}']").set(3)
+    #find("input[data-video-id='#{futurama.id}']").set(2)
+    #find("input[data-video-id='#{south_park.id}']").set(1)
 
     click_button 'Update Instant Queue'
+
     #using the ID-method
+
     #expect(find("video_#{south_park.id}").value).to eq('1')
     #expect(find("video_#{futurama.id}").value).to eq('2')
     #expect(find("video_#{monk.id}").value).to eq('3')
 
     #using the html5-data attr.
 
-    expect(find("input[data-video-id='#{south_park.id}']").value).to eq('1')
-    expect(find("input[data-video-id='#{futurama.id}']").value).to eq('2')
-    expect(find("input[data-video-id='#{monk.id}']").value).to eq('3')
+    #expect(find("input[data-video-id='#{south_park.id}']").value).to eq('1')
+    #expect(find("input[data-video-id='#{futurama.id}']").value).to eq('2')
+    #expect(find("input[data-video-id='#{monk.id}']").value).to eq('3')
 
+    # using Xpath
+
+    within(:xpath, "//tr[contains(.,'#{monk.title}')]")  do
+      fill_in "queue_items[][position]", with: 3
+    end
+
+    within(:xpath, "//tr[contains(.,'#{futurama.title}')]")  do
+      fill_in "queue_items[][position]", with: 2
+    end
+
+    within(:xpath, "//tr[contains(.,'#{south_park.title}')]")  do
+      fill_in "queue_items[][position]", with: 1
+    end
+
+    click_button "Update Instant Queue"
+
+    expect(find(:xpath, "//tr[contains(.,'#{south_park.title}')]//input[@type='text']").value).to eq('1')
+    expect(find(:xpath, "//tr[contains(.,'#{futurama.title}')]//input[@type='text']").value).to eq('2')
+    expect(find(:xpath, "//tr[contains(.,'#{monk.title}')]//input[@type='text']").value).to eq('3')
 
   end
 
