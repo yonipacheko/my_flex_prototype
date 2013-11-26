2b6a56f5b34227cc62d1ef5e7a32787b036bca93
yoni pacheko <fernando.lbarrajon8@gmail.com>
2013-11-26 16:42:00 +0100

    removing pry from Application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,5 @@
-require 'pry'
 module ApplicationHelper
   def options_for_video_reviews(selected = nil)
-    #binding.pry
     options_for_select([5, 4, 3, 2, 1].map{ |number|
     [pluralize(number, 'Star'), number]}, selected)
 
