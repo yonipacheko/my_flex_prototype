d608fc039a78cff1022f2a5e81fd1f7684cc3e7c
yoni pacheko <fernando.lbarrajon8@gmail.com>
2013-12-05 18:25:50 +0100

    editing mailGun settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Myflix::Application.configure do
       :address        => ENV['MAILGUN_SMTP_SERVER'],
       :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
       :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-      :domain         => 'yourapp.heroku.com',
+      :domain         => 'secret-sea-6367.herokuapp.com',
       :authentication => :plain,
   }
 
