3b2fc1a93f22dc7f0fba639416b87b1465677788
yoni pacheko <fernando.lbarrajon8@gmail.com>
2013-12-19 22:40:44 +0100

    wip

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,11 +5,9 @@ CarrierWave.configure do |config|
         :provider               => 'AWS',                        # required
         :aws_access_key_id      =>  ENV['S3_KEY'],               # required
         :aws_secret_access_key  => ENV['S3_SECRET'],                        # required
-        :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-        :host                   => 's3.example.com',             # optional, defaults to nil
-        :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+
     }
-    config.fog_directory  = 'filmbox'                     # required
+    config.fog_directory  = 'filmbox_1'                     # required
   else
     config.storage = :file
     config.enable_processing = Rails.env.development?
