6cbbc48d356f44541e7ca4a5d9ff24e5ef315689
yoni pacheko <fernando.lbarrajon8@gmail.com>
2014-01-07 17:20:01 +0100

    wip

--- app/models/stripe_wrapper.rb
+++ app/models/stripe_wrapper.rb
@@ -1,9 +1,12 @@
   module StripeWrapper
-
     class Charge
-      def initialize(response, error_message)
-        @response = response
-        @error_message = error_message
+
+      attr_reader :error_message, :response
+
+
+      def initialize(options = {})
+        @response = options[:response]
+        @error_message = options[:error_message]
       end
       def self.create( options = {} )
         begin
@@ -13,19 +16,53 @@
               card: options[:card],
               description: options[:description]
           )
-          Charge.new(response, nil) # the same like just writing: new(response)
+          Charge.new(response: response) # the same like just writing: new(response)
         rescue Stripe::CardError => e
-          Charge.new(nil, e.message)
+          Charge.new(error_message: e.message)
         end
       end
 
       def successful?
-        @response.present?
+        response.present?  # is this really necessary here?
       end
 
-      def error_message
-        @error_message
-      end
+      #check attr_reader!
+
+      #def error_message
+      #  @error_message
+      #end
 
     end
   end
+
+
+
+  # the first version of this class
+  #class Charge
+  #  def initialize(response, error_message)
+  #    @response = response
+  #    @error_message = error_message
+  #  end
+  #  def self.create( options = {} )
+  #    begin
+  #      response = Stripe::Charge.create(
+  #          amount: options[:amount],
+  #          currency: 'usd',
+  #          card: options[:card],
+  #          description: options[:description]
+  #      )
+  #      Charge.new(response, nil) # the same like just writing: new(response)
+  #    rescue Stripe::CardError => e
+  #      Charge.new(nil, e.message)
+  #    end
+  #  end
+  #
+  #  def successful?
+  #    @response.present?
+  #  end
+  #
+  #  def error_message
+  #    @error_message
+  #  end
+  #
+  #end
--- spec/features/user_invites_friend_spec.rb
+++ spec/features/user_invites_friend_spec.rb
@@ -46,7 +46,7 @@ feature 'User invites Friend' do
     fill_in 'Credit Card Number', with: '4242424242424242'
     fill_in 'Security Code', with: '123'
     select '12 - December', from: 'date_month'
-    select '2013', from: 'date_year'
+    select '2014', from: 'date_year'
     click_button "Sign Up"
   end
 
